1cbaf2087b40149e17866b27980ac55a02db9d38
sstankevych999 <serhii_stankevych@epam.com>
2022-10-22 00:36:54 +0300

    lesson 3 add movement w key

--- MyGame/my_game.py
+++ MyGame/my_game.py
@@ -16,8 +16,6 @@ LIGHT_YELLOW = (255, 255, 204)
 MINT = (62, 180, 137)
 BROWN = (150, 75, 0)
 
-
-
 GAME_WIN = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGTH))
 pygame.display.set_caption("eKids Game")
 done = False
@@ -39,10 +37,16 @@ while not done:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 speed_x = -5
+            if event.key == pygame.K_RIGHT:
+                speed_x = 5
+            if event.key == pygame.K_UP:
+                speed_y = -3
+            if event.key == pygame.K_DOWN:
+                speed_y = 3
+
         elif event.type == pygame.KEYUP:
             speed_x = 0
             speed_y = 0
-
 
     cord_x = cord_x + speed_x
     cord_y = cord_y + speed_y
